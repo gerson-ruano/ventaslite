c1ad65e1afd0150d7cff26dd80ebc1e0b0c9cafb
gersonruano <toge619@gmail.com>
2023-02-18 18:37:34 -0600

    tabla principal, dashboard vid-23

---
+++ app/Http/Livewire/Categories.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Category;
+use Illuminate\Support\Facades\Storage;
+
+use Livewire\WithFileUploads;
+use Livewire\WithPagination;
+
+class Categories extends Component
+{
+    use WithFileUploads;
+    use WithPagination;
+
+    public $name, $search, $image, $selected_id, $pageTitle, $componentName;
+    private $pagination = 5;
+
+    public function mount()
+    {
+        $this->pageTitle = 'Listado';
+        $this->componentName = 'Categorias';
+    }
+
+    public function render()
+    {
+        $data = Category::all();
+
+        return view('livewire.category.categories', ['categories' => $data])
+        ->extends('layouts.theme.app')
+        ->section('content');
+    }
+}
--- resources/views/layouts/theme/header.blade.php
+++ resources/views/layouts/theme/header.blade.php
@@ -4,7 +4,7 @@
         <ul class="navbar-item flex-row">
             <li class="nav-item theme-logo">
                 <a href="index.html">
-                    <img src="assets/img/90x90.jpg" class="navbar-logo" alt="logo">
+                    <img src="assets/img/ventas.png" class="navbar-logo" alt="logo"><b style="font-size: 19px: color:#3B3F5C">LWPOS</b>
                 </a>
             </li>
         </ul>
@@ -26,12 +26,14 @@
 
             <li class="nav-item dropdown user-profile-dropdown  order-lg-0 order-1">
                 <a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="userProfileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="assets/img/ca.png" alt="admin-profile" class="img-fluid">
+                    <!--img src="assets/img/ca.png" alt="admin-profile" class="img-fluid"-->
+                    <i class="fas fa-user text-dark"></i>
                 </a>
                 <div class="dropdown-menu position-absolute animated fadeInUp" aria-labelledby="userProfileDropdown">
                     <div class="user-profile-section">
                         <div class="media mx-auto">
-                            <img src="assets/img/ca.png" class="img-fluid mr-2" alt="avatar">
+                            <img src="assets/img/lara.png" class="img-fluid mr-2" alt="avatar">
+
                             <div class="media-body">
                                 <h5>Mi Perfil</h5>
                                 <p>Desarrollador</p>
@@ -40,13 +42,21 @@
                     </div>
                     <div class="dropdown-item">
                         <a href="user_profile.html">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> <span>Mi Perfil</span>
+                            <!--svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg--> 
+                            <i class="fas fa-user"></i>
+                            <span>Mi Perfil</span>
                         </a>
                     </div>
                     <div class="dropdown-item">
-                        <a href="auth_login.html">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg> <span>Log Out</span>
+                        <a href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit()"
+                            > 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg> 
+                            <span>Log Out</span>
                         </a>
+                        <form action="{{ route('logout') }}" method="POST" id="logout-form">
+                            @csrf
+                        </form>
                     </div>
                 </div>
             </li>
