98a4da4c4198e563a2858de26e8932408ed2ef6d
gersonruano <toge619@gmail.com>
2023-04-14 15:17:12 -0600

    creacion, edicion, borrado de categorias, con sus respectivos metodos

--- app/Http/Livewire/Categories.php
+++ app/Http/Livewire/Categories.php
@@ -53,11 +53,102 @@ class Categories extends Component
 
     public function Store()
     {
-        
+        $rules = [
+            'name' => 'required|unique:categories|min:3'
+        ];
+
+        $messages = [
+            'name.required' => 'Nombre de la categoria es requerida',
+            'name.unique' => 'Ya existe el nombre de la categoria',
+            'name.min' => 'El nombre de la categoria debe tener al menos 3 caracteres'
+        ];
+
+        $this->validate($rules, $messages);
+
+        $category = Category::create([
+            'name' => $this->name
+        ]);
+
+        $customFileName;
+        if($this->image)
+        {
+            $customFileName = uniqid() . '_.' . $this->image->extension();
+            $this->image->storeAs('public/categories', $customFileName);
+            $category->image = $customFileName;
+            $category->save();
+        }
+
+        $this->resetUI();
+        $this->emit('category-added', 'Categoria Registrada');
+    }
+
+    public function Update()
+    {
+        $rules = [
+            'name' => "required|min:3|unique:categories,name,{$this->selected_id}"
+        ];
+
+        $messages = [
+            'name.required' => 'Nombre de Categoria Requerido',
+            'name.min' => 'El nombre de la categoria debe tener al menos 3 caracteres',
+            'name.unique' => 'El nombre de la categoria ya existe'
+        ];
+
+        $this->validate($rules, $messages);
+
+        $category = Category::find($this->selected_id);
+        $category->update([
+            'name' => $this->name
+        ]);
+
+        if($this->image)
+        {
+            $customFileName = uniqid() . '_.' . $this->image->extension();
+            $this->image->storeAs('public/categories', $customFileName);
+
+            $imageName = $category->image;
+            $category->image = $customFileName;
+            $category->save();
+
+            if($imageName !=null)
+            {
+                if(file_exists('storage/categories' . $imageName))
+                {
+                    unlink('storage/categories' . $imageName);
+                }
+            }
+        }
+        $this->resetUI();
+        $this->emit('category-updated', 'Categoria Actualizada');
+
     }
 
     public function resetUI()
     {
+        $this->name = '';
+        $this->image = null;
+        $this->search = '';
+        $this->selected_id = 0;
+    }
 
+    protected $listeners = [
+        'deleteRow' => 'Destroy'
+    ];
+
+    public function Destroy(Category $category)
+    {
+        //$category = Category::find($id);
+        //dd($category);
+
+        $imageName = $category->image; //imagen temporal
+        $category->delete();
+
+        if($imageName !=null)
+        {
+            unlink('storage/categories/' . $imageName);
+
+        }
+        $this->resetUI();
+        $this->emit('category-deleted', 'Categoria Eliminada');
     }
 }
--- resources/views/livewire/category/categories.blade.php
+++ resources/views/livewire/category/categories.blade.php
@@ -1,18 +1,19 @@
 <div class="row sales layout-top-spacing">
-    <div class="col-sm-12">
 
+    <div class="col-sm-12">
         <div class="widget widget-chart-one">
             <div class="widget-heading">
                 <h4 class="card-title">
-                    <b>{{$componentName}} | {{$pageTitle}}</b>
+                    <b>{{ $componentName }} | {{ $pageTitle }}</b>
                 </h4>
                 <ul class="tabs tab-pills">
                     <li>
-                        <a href="javascript:void(0)" class="tabmenu bg-dark" data-toggle="modal" 
-                        data-target="#theModal">Agregar</a>
+                        <a href="javascript:void(0)" class="tabmenu bg-dark" data-toggle="modal"
+                            data-target="#theModal">Agregar</a>
                     </li>
                 </ul>
             </div>
+
             @include('common.searchbox')
 
             <div class="widget-content">
@@ -27,31 +28,35 @@
                         </thead>
                         <tbody>
                             @foreach ($categories as $category)
-                            <tr>
-                                <td><h6>{{$category->name}}</h6></td>
-                                <td class="text-center">
-                                    <span>
-                                        <img src="{{ asset('storage/categories/' .$category->image) }}" alt="imagen de ejemplo" height="70" width="80" class="rounded">
-                                    </span>
-                                </td>
-                                <td class="text-center">
-                                    <a href="javascript:void(0)"
-                                    wire:click="Edit({{$category->id}})" 
-                                    class="btn btn-dark mtmobile" title="Edit">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                    <a href="javascript:void(0)" 
-                                    onclick="Confirm('{{$category->id}}')"
-                                    class="btn btn-dark " title="Delete">
-                                        <i class="fas fa-trash"></i>
-                                    </a>
+                                <tr>
+                                    <td>
+                                        <h6>{{ $category->name }}</h6>
+                                    </td>
+                                    <td class="text-center">
+                                        <span>
+                                            <img src="{{ asset('storage/categories/' . $category->image) }}"
+                                                alt="imagen de ejemplo" height="70" width="80" class="rounded">
+                                        </span>
+                                    </td>
+                                    <td class="text-center">
+                                        <a href="javascript:void(0)" wire:click="Edit({{ $category->id }})"
+                                            class="btn btn-dark mtmobile" title="Edit">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
 
-                                </td>
-                            </tr>
+
+                                        <a href="javascript:void(0)"
+                                            onclick="Confirm('{{ $category->id }}','{{ $category->products->count() }}')"
+                                            class="btn btn-dark " title="Delete">
+                                            <i class="fas fa-trash"></i>
+                                        </a>
+
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
-                    {{$categories->links()}}
+                    {{ $categories->links() }}
                 </div>
 
             </div>
@@ -62,10 +67,40 @@
 </div>
 
 <script>
-    document.addEventListener('DOMContentLoaded', function(){
+    document.addEventListener('DOMContentLoaded', function() {
 
         window.livewire.on('show-modal', msg => {
             $('#theModal').modal('show');
         });
+        window.livewire.on('category-added', msg => {
+            $('#theModal').modal('hide');
+        });
+        window.livewire.on('category-updated', msg => {
+            $('#theModal').modal('hide');
+        });
     });
+
+    function Confirm(id, products) {
+
+        if (products > 0) {
+            swal('No se puede eliminar la categoria, porque tiene productos existentes')
+            return;
+        }
+        swal({
+            title: "QUE DESEA REALIZAR?",
+            //text: "You will not be able to recover this imaginary file!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "SI, ELIMINAR!",
+            closeOnConfirm: false
+        }).then(function(result) {
+            if (result.value) {
+                window.livewire.emit('deleteRow', id)
+                //swal("Deleted!", "Your imaginary file has been deleted.", "success");
+                swal.close()
+            }
+        });
+
+    }
 </script>
